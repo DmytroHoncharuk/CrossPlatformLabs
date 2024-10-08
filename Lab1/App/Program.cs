3f9e567b1667db7547fea974692a34e4909c548c
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-10-08 18:55:51 +0300

    add Program.cs file

--- Lab1/App/Program.cs
+++ Lab1/App/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace App
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            int numberOflevels;
+            try
+            {
+                numberOflevels = IOHelper.ReadSingleNumberFromFile();
+                Console.WriteLine($"Number of Levels: {numberOflevels}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            int countOfTriangles;
+            try
+            {
+                countOfTriangles = TrianglesService.GetNumberOfTriangles(numberOflevels);
+                Console.WriteLine($"Count of triangles: {countOfTriangles}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            try
+            {
+                IOHelper.WriteResultToFile(countOfTriangles);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
