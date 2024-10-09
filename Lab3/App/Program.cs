212b728d6554889c9da36d331a94ba146a6c0102
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-10-09 22:08:58 +0300

    updated Program file

--- Lab3/App/Program.cs
+++ Lab3/App/Program.cs
@@ -8,21 +8,17 @@ namespace App
         {
             try
             {
-                // Читання даних з файлу через IOHelper
                 var (n, edges) = IOHelper.ReadGraphDataFromFile();
 
-                // Обробка графа через GraphService
                 string result = GraphService.ProcessGraph(n, edges);
 
-                // Оскільки виведення повинно бути тільки "Yes" або "No",
                 string finalResult = result.StartsWith("Yes") ? "Yes" : "No";
 
-                // Запис результату у файл через IOHelper
+                Console.WriteLine(finalResult);
                 IOHelper.WriteResultToFile(finalResult);
             }
             catch (Exception ex)
             {
-                // У випадку помилки, записуємо "No" у файл
                 IOHelper.WriteResultToFile("No");
                 Console.WriteLine($"Помилка: {ex.Message}");
             }
