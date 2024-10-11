4fd0bdb2cd83d3f709edfd6c9eb8dd699040494a
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-10-11 20:33:16 +0300

    add input file and Program file

--- Lab2/App/Program.cs
+++ Lab2/App/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace App;
+
+public class Program
+{
+    private static void Main()
+    {        
+        int numberOfLevels;
+        try
+        {
+            numberOfLevels = IOHelper.ReadSingleNumberFromFile();  
+            Console.WriteLine($"Number of Levels: {numberOfLevels}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading input file: {e.Message}");
+            return;
+        }
+
+        int countOfWaysToBuildTower;
+        try
+        {
+            countOfWaysToBuildTower = GlassProblemSolver.Solve(numberOfLevels);  
+            Console.WriteLine($"Number of ways to build the tower: {countOfWaysToBuildTower}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error solving the problem: {e.Message}");
+            return;
+        }
+
+        try
+        {
+            IOHelper.WriteResultToFile(countOfWaysToBuildTower);  
+            Console.WriteLine("Result successfully written to OUTPUT.TXT");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error writing to output file: {e.Message}");
+        }
+    }
+}
